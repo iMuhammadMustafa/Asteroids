7da5af39354dd085d4a7cd4099b7e36a341ee78f
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-07-10 11:28:12 +0300

    Draw Center of the player

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const height = (canvas.height = window.innerHeight);
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.radius = 10;
+    this.rotation = 0;
+  }
+
+  draw() {
+    //Draw Center
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 3, 0, 2 * Math.PI, false);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+const player = new Player({ position: { x: width / 2, y: height / 2 }, velocity: { vx: 0, vy: 0 } });
+player.draw();
